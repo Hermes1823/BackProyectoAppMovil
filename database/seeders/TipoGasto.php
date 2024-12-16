3f431537b2ef19423155c332cd9b1eee89ee58e2
Edisson Galvez <edigalmor@outlook.com>
2024-12-16 08:16:06 -0500

    Spent

--- database/seeders/TipoGasto.php
+++ database/seeders/TipoGasto.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
 
 class TipoGasto extends Seeder
 {
@@ -12,6 +14,23 @@ class TipoGasto extends Seeder
      */
     public function run(): void
     {
-        //
+        DB::table('tipogastos')->insert([
+            [
+                'IdTipogasto' => 1,
+                'Descripcion' => 'Transporte',
+            ],
+            [
+                'IdTipogasto' => 2,
+                'Descripcion' => 'Alimentación',
+            ],
+            [
+                'IdTipogasto' => 3,
+                'Descripcion' => 'Hospedaje',
+            ],
+            [
+                'IdTipogasto' => 4,
+                'Descripcion' => 'Otros',
+            ],
+        ]);
     }
 }
